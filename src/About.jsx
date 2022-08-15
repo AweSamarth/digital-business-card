f91ae894e18b0ae970217a3bbda8866b0d2d1bc9
Samarth Saxena <samarthsaxena1672003@gmail.com>
2022-08-15 22:20:01 +0530

    almost done (?)

--- src/About.jsx
+++ src/About.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 export default function About(){
 
     return(
-        <div className='component' id="about">Another check</div>
+        <div className='component' id="about">
+            <div id="abouttext">About</div>
+            <div id="othertext">I am a frontend developer and a technical writer. I am extremely interested in web3 and thus
+            my final goal is to become a blockchain developer or a DevRel  </div> 
+        </div>
+
     )
 }
--- src/Interests.jsx
+++ src/Interests.jsx
@@ -2,6 +2,11 @@ import React from "react"
 
 export default function Interests(){
     return(
-        <div className='component' id="interests" >Interests Section</div>
+        <div className='component' id="interests" >
+            <div id="interesthead">Interests</div>
+            <div id="myinterests">I like building cool stuff and playing video games. Pav bhaji enjoyer, especially when they are made by
+            yo mom</div>
+        </div>
+        
     )
 }
