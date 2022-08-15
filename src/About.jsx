a8a0b961e5018dd029c2a66e84c0838f749648ad
Samarth Saxena <samarthsaxena1672003@gmail.com>
2022-08-15 23:16:01 +0530

    done!

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ export default function About(){
     return(
         <div className='component' id="about">
             <div id="abouttext">About</div>
-            <div id="othertext">Frontend developer and a technical writer. CEO of Sax(ena) and Head of the Based Department (2003-present)  </div> 
+            <div id="othertext">Frontend developer and technical writer. CEO of Sax(ena) and Head of the Based Department (2003-present)  </div> 
         </div>
 
     )
